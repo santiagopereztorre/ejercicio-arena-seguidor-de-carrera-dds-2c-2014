3da3cd026e21572da34502ad1df920e9224e4edd
santiagopereztorre <saantii.28@hotmail.com>
2014-08-26 12:54:13 -0300

    Primera parte de la view del seguidor de carrera

--- src/main/java/ui/SeguidorView.java
+++ src/main/java/ui/SeguidorView.java
@@ -1,22 +1,39 @@
 package ui;
 
+import org.uqbar.arena.layout.HorizontalLayout;
+import org.uqbar.arena.layout.VerticalLayout;
+import org.uqbar.arena.widgets.Button;
+import org.uqbar.arena.widgets.CheckBox;
+import org.uqbar.arena.widgets.Label;
+import org.uqbar.arena.widgets.List;
 import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.widgets.TextBox;
 import org.uqbar.arena.windows.MainWindow;
 
 import domain.UnModel;
 
 //IMPORTANTE: correr con -Djava.system.class.loader=com.uqbar.apo.APOClassLoader
-public class UnaView extends MainWindow<UnViewModel> {
+public class SeguidorView extends MainWindow<UnViewModel> {
 
-  public UnaView() {
+  public SeguidorView() {
     super(new UnViewModel(new UnModel()));
   }
 
   @Override
   public void createContents(Panel mainPanel) {
+	  
+	  Panel primerPanel = new Panel(mainPanel);
+	  primerPanel.setLayout(new HorizontalLayout());
+	  Panel segundoPanel = new Panel(mainPanel);
+	  segundoPanel.setLayout(new VerticalLayout());
+
+	  new Label(primerPanel).setText("Seguidor de carrera");
+	  new Label(segundoPanel).setText("Materias");
+	  new List(segundoPanel);
+	  new Button(segundoPanel).setCaption("Nueva materia");
   }
 
   public static void main(String[] args) {
-    new UnaView().startApplication();
+    new SeguidorView().startApplication();
   }
 }
