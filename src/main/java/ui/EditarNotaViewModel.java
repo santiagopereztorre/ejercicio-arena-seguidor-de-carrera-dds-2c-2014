a0c5739c81e5c2fcb57352d4572f5db9d66d4930
santiagopereztorre <saantii.28@hotmail.com>
2014-08-26 22:49:23 -0300

    Agrego vista controlador de editar nota

---
+++ src/main/java/ui/EditarNotaViewModel.java
@@ -0,0 +1,39 @@
+package ui;
+
+import org.uqbar.commons.utils.Observable;
+
+@Observable
+public class EditarNotaViewModel {
+	private String fecha;
+	private String descripcion;
+	private Boolean aprobado;
+	
+	public String getFecha() {
+		return fecha;
+	}
+	
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public Boolean getAprobado() {
+		return aprobado;
+	}
+	
+	public void setAprobado(Boolean aprobado) {
+		this.aprobado = aprobado;
+	}
+	
+	public void editar() {
+		
+	}
+
+}
