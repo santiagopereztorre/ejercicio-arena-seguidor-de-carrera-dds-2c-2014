ea8258513e4fe838d2fe177e247f8dff5b8bc6e0
santiagopereztorre <saantii.28@hotmail.com>
2014-08-27 16:39:44 -0300

    agrego metodo delete a materiasHome

--- src/main/java/domain/MateriasHome.java
+++ src/main/java/domain/MateriasHome.java
@@ -3,18 +3,24 @@ package domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.uqbar.edu.paiu.examples.celulares.domain.Celular;
+
 public class MateriasHome {
 	private static MateriasHome instance;
 	private List<Materia> materias = new ArrayList<Materia>();
-	
+
 	public static synchronized MateriasHome getInstance() {
 		if (instance == null) {
 			instance = new MateriasHome();
 		}
 		return instance;
 	}
-	
-	public void add(Materia materia){
+
+	public void add(Materia materia) {
 		materias.add(materia);
+	}
+
+	public void delete(Materia materia) {
+		this.materias.remove(materia);
 	}
 }
